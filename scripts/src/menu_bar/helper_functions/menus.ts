75d4a22453da9222442b27f419729201b71cd9c8
Charlie-Sumorok <77081751+Charlie-Sumorok@users.noreply.github.com>
2021-03-10 07:59:19 -0500

    Create menus.ts

---
+++ scripts/src/menu_bar/helper_functions/menus.ts
@@ -0,0 +1,12 @@
+import { Menu, MenuItemConstructorOptions } from 'electron';
+
+/** Some menu */
+type MainMenu = MenuItemConstructorOptions;
+
+/** Some submenu */
+type SubMenu = MainMenu[] | Menu;
+
+export {
+	MainMenu,
+	SubMenu,
+};
