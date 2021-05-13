5aad693494eb5801aa1a6673ad2f54d81fce2932
Charlie-Sumorok <77081751+Charlie-Sumorok@users.noreply.github.com>
2021-05-13 17:49:55 -0400

    Update template.ts

--- scripts/src/menu_bar/template.ts
+++ scripts/src/menu_bar/template.ts
@@ -28,41 +28,6 @@ const bug_report = new GitHubIssue({
 	title: 'Bug+Report',
 });
 
-const bug_report_body = `
----
-name: Bug Report
-about: Create a report to help us improve
-title: Bug Report
-labels: bug
-assignees: ''
-
----
-
-**Describe the bug**
-<!-- A clear and concise description of what the bug is. -->
-
-**To Reproduce**
-<!-- Steps to reproduce the behavior -->
-1. Go to '...'
-2. Click on '....'
-3. Scroll down to '....'
-4. See error
-
-**Expected behavior**
-<!-- A clear and concise description of what you expected to happen. -->
-
-**Screenshots**
-<!-- If applicable, add screenshots to help explain your problem. -->
-
-**Desktop (please complete the following information):**
- - OS: <!-- [e.g. iOS] -->
- - App Version <!-- [e.g. 22] -->
-
-**Additional context**
-<!-- Add any other context about the problem here. -->
-
-${debugInfo()}`;
-
 const helpSubmenu: SubMenu = [
 	gitHubRepo_MenuBar_Item({
 		label: 'Website',
