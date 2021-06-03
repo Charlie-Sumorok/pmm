046fe67bd60c33346f9da89c87dfe2a955bf5530
Charlie-Sumorok <77081751+Charlie-Sumorok@users.noreply.github.com>
2021-06-03 13:47:05 -0400

    Update home.py

--- cli/commands/home.py
+++ cli/commands/home.py
@@ -10,12 +10,11 @@ def parse_args(args):
 	'parse arguments'
 	if len(args) == 0:
 		url = 'https://github.com/Charlie-Sumorok/PMM'
-		open_url(url)
 	else:
 		package_manager_name = args[0]
 		url = 'https://raw.githubusercontent.com/Charlie-Sumorok/PMM/main/package-managers/data.json'
 		package_manager_data = requests.get(url).json()
 		package_manager = package_manager_data[package_manager_name]
 		urls = package_manager['urls']
-		home_url = urls['standard']
-		open_url(home_url)
+		url = urls['standard']
+	open_url(url)
